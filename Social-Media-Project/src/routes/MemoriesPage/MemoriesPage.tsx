4cae01191e97380a91a465f8e371b51ed68cd88f
AurimasB <aurimas.buta@zenitech.co.uk>
2024-04-12 16:27:24 +0300

    Created a base for Table component and added Link for Generic Button component

--- Social-Media-Project/src/routes/MemoriesPage/MemoriesPage.tsx
+++ Social-Media-Project/src/routes/MemoriesPage/MemoriesPage.tsx
@@ -1,7 +1,13 @@
+import Table from "../../components/Others/Table/Table";
+import Button from "../../components/Others/Button/Button";
+
 export default function MemoriesPage(){
     return(
         <main>
             <h1>My Memories</h1>
+            <Button innerText={"Back to Main Page"}
+                    link={"/"}/>
+            <Table/>
         </main>
     );
 }
