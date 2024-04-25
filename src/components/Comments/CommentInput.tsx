5a040ae0967114f1f4a690707ff268a90d1ee0a1
AurimasB <77635359+AurimasB@users.noreply.github.com>
2024-04-25 19:07:58 +0300

    Styled comment input field

--- src/components/Comments/CommentInput.tsx
+++ src/components/Comments/CommentInput.tsx
@@ -2,9 +2,16 @@ import "./commentInput.css";
 
 export default function CommentInput() {
   return (
-    <>
-      <input type="text" className="addComment" />
-      <button className="postComment">Send</button>
-    </>
+    <div className="commentInputSection">
+      <input
+        type="text"
+        className="addComment"
+        placeholder="Add your comment..."
+      />
+      <button className="postComment">
+        <p>Comment</p>
+        <img src="./icons/rightArrow-purple.png" alt="Comment submit icon" />
+      </button>
+    </div>
   );
 }
