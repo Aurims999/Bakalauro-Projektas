de0c681deaae0ccc19a33303967cc4a17d979a4e
AurimasB <77635359+AurimasB@users.noreply.github.com>
2024-05-03 17:37:33 +0300

    Added visuals for the sign in page

--- src/routes/LoginPage/SideVisuals/SideVisuals.tsx
+++ src/routes/LoginPage/SideVisuals/SideVisuals.tsx
@@ -1,14 +1,15 @@
 import { Outlet } from "react-router-dom";
 
+import "../loginPage.css";
 import "./sideVisuals.css";
 
 export default function SideVisuals() {
   return (
-    <>
+    <div className="sign-in-container">
       <section className="signin-visuals">
-        <h1>Side Visuals</h1>
+        <img src="./images/visuals/signin-page.png" alt="Side visuals" />
       </section>
       <Outlet />
-    </>
+    </div>
   );
 }
