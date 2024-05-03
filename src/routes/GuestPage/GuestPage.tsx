d0dcf6a53eea1697ac557e4574d53e3bea8add31
AurimasB <77635359+AurimasB@users.noreply.github.com>
2024-05-03 15:03:25 +0300

    Finished visuals for guest page

--- src/routes/GuestPage/GuestPage.tsx
+++ src/routes/GuestPage/GuestPage.tsx
@@ -1,10 +1,33 @@
 import "./guestPage.css";
+
 import Header from "../../components/Header/Header";
+import Button from "../../components/Others/Button/Button";
 
 export default function MessagesPage() {
   return (
     <div className="guestPageContainer">
       <Header />
+      <div className="guestPageMainContent">
+        <section className="textBlock">
+          <h1>
+            Trip
+            <span style={{ color: "var(--main__purple)" }}> Experience </span>
+            <span style={{ color: "var(--main__orange)" }}>Sharing </span>
+            website by people for people
+          </h1>
+          <h2>
+            Share your trip experience with your friends and find some
+            inspiration for your next trip!
+          </h2>
+          <Button innerText={"Try it Out"} />
+        </section>
+        <section className="visuals">
+          <img
+            src="./images/visuals/guestpage-visuals.png"
+            alt="guest page visuals"
+          />
+        </section>
+      </div>
     </div>
   );
 }
