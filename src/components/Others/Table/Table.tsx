eb07751ae89595d1763b948041a902b4cf60746c
AurimasB <77635359+AurimasB@users.noreply.github.com>
2024-05-09 15:05:54 +0300

    Created a base for comments table

--- src/components/Others/Table/Table.tsx
+++ src/components/Others/Table/Table.tsx
@@ -1,8 +1,120 @@
-import "./table.css"
+import "./table.css";
 
-export default function Table(){
-    return(
-        <div className="dataTable">
+export default function Table() {
+  return (
+    <div className="dataTable">
+      <div className="tableContainer">
+        <div className="table-header">
+          <h2>Memory</h2>
+          <h2>Title</h2>
+          <h2>Comment</h2>
+          <h2>Status</h2>
         </div>
-    );
+        <div className="table-content">
+          <div className="table-row">
+            <div className="table-row-block memoryPreview">
+              <img
+                src="./images/memories/c9200a84-aa26-4d54-afe7-90e928fa15a1.png"
+                alt="Memory preview image"
+              />
+            </div>
+            <div className="table-row-block">
+              <p>Title</p>
+            </div>
+            <div className="table-row-block">
+              <p>"Comment"</p>
+            </div>
+            <div className="table-row-block">
+              <div className="statusBlock">Approved</div>
+            </div>
+          </div>
+          <div className="table-row">
+            <div className="table-row-block memoryPreview">
+              <img
+                src="./images/memories/c9200a84-aa26-4d54-afe7-90e928fa15a1.png"
+                alt="Memory preview image"
+              />
+            </div>
+            <div className="table-row-block">
+              <p>Title</p>
+            </div>
+            <div className="table-row-block">
+              <p>"Comment"</p>
+            </div>
+            <div className="table-row-block">
+              <div className="statusBlock">Approved</div>
+            </div>
+          </div>
+          <div className="table-row">
+            <div className="table-row-block memoryPreview">
+              <img
+                src="./images/memories/c9200a84-aa26-4d54-afe7-90e928fa15a1.png"
+                alt="Memory preview image"
+              />
+            </div>
+            <div className="table-row-block">
+              <p>Title</p>
+            </div>
+            <div className="table-row-block">
+              <p>"Comment"</p>
+            </div>
+            <div className="table-row-block">
+              <div className="statusBlock">Approved</div>
+            </div>
+          </div>
+          <div className="table-row">
+            <div className="table-row-block memoryPreview">
+              <img
+                src="./images/memories/c9200a84-aa26-4d54-afe7-90e928fa15a1.png"
+                alt="Memory preview image"
+              />
+            </div>
+            <div className="table-row-block">
+              <p>Title</p>
+            </div>
+            <div className="table-row-block">
+              <p>"Comment"</p>
+            </div>
+            <div className="table-row-block">
+              <div className="statusBlock">Approved</div>
+            </div>
+          </div>
+          <div className="table-row">
+            <div className="table-row-block memoryPreview">
+              <img
+                src="./images/memories/c9200a84-aa26-4d54-afe7-90e928fa15a1.png"
+                alt="Memory preview image"
+              />
+            </div>
+            <div className="table-row-block">
+              <p>Title</p>
+            </div>
+            <div className="table-row-block">
+              <p>"Comment"</p>
+            </div>
+            <div className="table-row-block">
+              <div className="statusBlock">Approved</div>
+            </div>
+          </div>
+          <div className="table-row">
+            <div className="table-row-block memoryPreview">
+              <img
+                src="./images/memories/c9200a84-aa26-4d54-afe7-90e928fa15a1.png"
+                alt="Memory preview image"
+              />
+            </div>
+            <div className="table-row-block">
+              <p>Title</p>
+            </div>
+            <div className="table-row-block">
+              <p>"Comment"</p>
+            </div>
+            <div className="table-row-block">
+              <div className="statusBlock">Approved</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
